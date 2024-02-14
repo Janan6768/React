e9020a2a0ea0caba9f3784cf4b8ef88f2ab6654d
Janan6768 <02.95.janan@gmail.com>
2024-02-15 00:04:23 +0500

    update

--- memescreator/src/components/Memes.jsx
+++ memescreator/src/components/Memes.jsx
@@ -14,9 +14,18 @@ export const Memes = () => {
         const memesArray = allMememImage.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
+        
         setMeme(prevMeme =>({
                 ...prevMeme,
                 randomImage:url
+                
+        }))
+    }
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            [name]:value
         }))
     }
   return (
@@ -26,12 +35,16 @@ export const Memes = () => {
                 type="text"
                 placeholder="Top text"
                 className="form-input"
-            />
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}            />
             <input 
                 type="text"
                 placeholder="Bottom text"
                 className="form-input"
-            />
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}            />
             <button 
                 className="form-button"
                 onClick={getMemeImage}
@@ -39,7 +52,11 @@ export const Memes = () => {
                 Get a new meme image 🖼
             </button>
         </div>
+        <div className='meme'>
         <img src={meme.randomImage} className="meme-image" />
+        <h2 className='meme-text top'>{meme.topText}</h2>
+        <h2 className='meme-text bottom'>{meme.bottomText}</h2>
+        </div>
     </main>
   )
 }
